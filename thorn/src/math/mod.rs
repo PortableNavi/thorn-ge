7eac1f47b65603a9f955b0b95cef0ca826a3c2b0
PortableNavi <portable-navi@proton.me>
2025-05-04 18:03:28 +0200

    added helper functions for randomness

--- thorn/src/math/mod.rs
+++ thorn/src/math/mod.rs
@@ -5,6 +5,8 @@ mod vec_aliases;
 mod vector;
 
 
+use std::ops::Range;
+
 pub use mat::*;
 pub use mat_aliases::*;
 pub use named_indices::*;
@@ -23,4 +25,21 @@ pub mod named_indices
     pub const G: usize = 1;
     pub const B: usize = 2;
     pub const A: usize = 3;
+}
+
+
+// Random. i'll use rand for now and wrap it in a stub interface
+// in case i want to change something about the randomnes later
+use rand::{Rng, distr::uniform::SampleUniform, seq::SliceRandom};
+
+
+pub fn shuffle<T>(slice: &mut [T])
+{
+    slice.shuffle(&mut rand::rng());
+}
+
+
+pub fn random<T: PartialOrd + SampleUniform>(range: Range<T>) -> T
+{
+    rand::rng().random_range(range)
 }
